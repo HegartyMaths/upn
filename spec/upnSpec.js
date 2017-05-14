f7445f1b341879b828dab75edcb1440ede877fe7
Daniel Keeble <danielkeeble94@googlemail.com>
2017-05-14 22:10:14 +0100

    Improve tests

--- spec/upnSpec.js
+++ spec/upnSpec.js
@@ -2,7 +2,7 @@ const UPN = require('../lib/upn');
 
 let upn;
 
-describe('UPN', () => {
+describe('Validating UPNs', () => {
   beforeEach(() => {
     upn = new UPN();
   });
@@ -10,7 +10,14 @@ describe('UPN', () => {
   it('should be able to validate given strings', () => {
     expect(upn.validate('')).toEqual(false);
     expect(upn.validate('an-invalid-string')).toEqual(false);
+    expect(upn.validate('A123456789123')).toEqual(false);
 
     expect(upn.validate('Z213282605052')).toEqual(true);
+  });
+
+  it('should only be passed a string to validate', () => {
+    expect(() => upn.validate()).toThrowError(TypeError);
+    expect(() => upn.validate([])).toThrowError(TypeError);
+    expect(() => upn.validate({})).toThrowError(TypeError);
   });
 });
